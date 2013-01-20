e561218fa333758b5c8bd89d35305dd39e62eb7e
Kasper van Veen <kasperr_15@hotmail.com>
2013-01-20 18:12:19 +0100

    PHP pagina's aangemaakt en db.php voor database connection gemaakt

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
+<?php
+include_once('db.php');
+
+$forum_results = mysql_query('SELECT * FROM Forums') or die(mysql_error());
+
+$page_title = "Forum index";
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>Forum index</title>
+    <title><?php echo $page_title; ?></title>
 	<link rel="stylesheet" type="text/css" href="./index_files/StandaardOpmaak.css">
 </head>
 
 <body>
 
-<h1 class="banner">Forum index</h1>
+<h1 class="banner"><?php echo $page_title; ?></h1>
 
 <div class="menu">
 	<a href = "index.html"> Forum </a> |
@@ -19,41 +26,40 @@
 	<a href = "contact.html"> Contact </a>
 </div>
 
+<?php
+while ($forum = mysql_fetch_array($forum_results)) {
+?>
 <table class="subject-table">
 <tbody>
     <tr>
-        <th width="50%">Subjects</th>
+        <th width="50%"><?php echo $forum['Forum_name'] ?></th>
         <th width="30%">Last post</th>
         <th width="10%">Topics</th>
         <th width="10%">Reactions</th>
     </tr>
-    <tr>
-        <td><a href="topics.html">Subject 1</a></td>
-        <td></td>
-        <td></td>
-        <td></td>
-    </tr>
-    <tr>
-        <td><a href="topics.html">Subject 2</a></td>
-        <td></td>
-        <td></td>
-        <td></td>
-    </tr>
-    <tr>
-        <td><a href="topics.html">Subject 3</a></td>
-        <td></td>
-        <td></td>
-        <td></td>
-    </tr>
-    <tr>
-        <td><a href="topics.html">Subject 4</a></td>
-        <td></td>
-        <td></td>
-        <td></td>
-    </tr>
-</tbody>
-</table>
+<?php
+    $query = 'SELECT * FROM Catagories WHERE Forum_ID = '.$forum['ID']
+           . ' ORDER BY Name';
+    $category_results = mysql_query($query) or die(mysql_error());
 
+    while ($category = mysql_fetch_array($category_results)) {
+?>
+    <tr>
+        <td><a href="topics.php?forum_id=<?php echo $category['ID']; ?>"><?php echo $category['Name']; ?></a></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+<?php
+    }
+?>
+</tbody>
+</table>
+<?php
+}
+?>
+
+<!--
 <table class="subject-table">
 <tbody>
     <tr>
@@ -88,6 +94,7 @@
     </tr>
 </tbody>
 </table>
+-->
 
 </body>
 </html>
