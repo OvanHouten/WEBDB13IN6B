f71e6113af1c309cc6fe4b225e4ce198f2560934
OvanHouten <olaf.ajax@hotmail.com>
2013-01-30 01:32:18 +0100

    my cat wrote this

--- editprofile.php
+++ editprofile.php
@@ -4,8 +4,6 @@ require 'menu.php';
 session_start();
 if(!isset($_SESSION['User_ID'])){
 	header("Location: http://webdb.science.uva.nl/webdb13IN6B/login.php");
-	$_SESSION['Error'] = "You need to log in to see this page";
-	exit;
 }
 
 $dbusername='webdb13IN6B';
@@ -57,30 +55,34 @@ $posts = $row2['COUNT(User_ID)']
 			color:white;
 			font-size:13pt;
 		}
-		.upperbar a:link {color:white;text-decoration: none;}     
-		.upperbar a:visited {color:white;text-decoration: none;} 
-		.upperbar a:hover {color:white;text-decoration: none;}  
-		.upperbar a:active {color:white;text-decoration: none;}
 		.column{
 			background-color:white;
 			padding-top:5px;
 			padding-left:5px;
-			height:110px;
+			height:115px;
 			width:*%;
 			color:black;
 			font-family:sans-serif;
 			font-size:13pt;
-			}
+		}
 		.infobox{
 			background-color:white;
 			padding-left:5px;
-			padding-top:5px;
+			padding-right:5px;
 			margin-left:20%;
 			height:155px;
 			width:*%;
 			color:black;
 			font-family:sans-serif;
 			font-size:10pt;
+		}
+		.textarea {
+			left:20%;
+			width:100%;
+			height:135px;
+			font-family:sans-serif;
+			font-size:10pt;
+			resize:none;
 		}
 	</style>
 </head>
@@ -92,6 +94,7 @@ $posts = $row2['COUNT(User_ID)']
 	?>
 	
 	<div style="box-shadow: 0px 5px 20px #888888;">
+	<form action="edit.php" method="post">
 		<div class="profilebar">
 			<center><?php echo $username ?><br><br>
 			<img src="steve.jpg" width="40px" height="40px"><br>
@@ -103,38 +106,37 @@ $posts = $row2['COUNT(User_ID)']
 			</p></center>
 		</div>
 
-		<div class="upperbar" align="right">
-			<a href="editprofile.php">Edit</a>
+		<div align="right" class="upperbar">
+			<input type="submit" value="submit"/>
 		</div>
 
-		<!--Let op, als de lijst langer word, moeten de hoogtes worden aangepast!!!-->
-		
 		<div class="column">
 			<table width="80%" style="font-size:12px;">
 			<tr>
-				<td style="padding-top:5px; padding-bottom:6px;" width="50%" ><b>In-game name:</b></td>
-				<td><?php echo $username ?></td>
+				<td width="50%"><b>In-game name:</b></td>
+				<td><input type="text" name="fname" value="<?php echo $username ?>"/></td>
 			</tr>
 			<tr>
-				<td style="padding-top:5px; padding-bottom:6px;" ><b>Firstname:</b></td>
-				<td><?php echo $fname ?></td>
+				<td><b>Firstname:</b></td>
+				<td><input type="text" name="fname" value="<?php echo $fname ?>" /></td>
 			</tr>
 			<tr>
-				<td style="padding-top:5px; padding-bottom:6px" ><b>Lastname:</b></td>
-				<td><?php echo $lname ?></td>
+				<td><b>Lastname:</b></td>
+				<td><input type="text" name="lname" value="<?php echo $lname ?>" /></td>
 			</tr>
 			<tr>
-				<td style="padding-top:5px; padding-bottom:6px" ><b>Bought Minecraft:</b></td>
-				<td>Yes</td>
+				<td><b>Bought Minecraft:</b></td>
+				<td><input type="text" name="lname" value="yes" /></td>
 			</tr>
-			
 			</table>
 		</div>
+		<!--Let op, als de lijst langer word, moeten de hoogtes worden aangepast!!!-->
 	
 		<div class="infobox">
 			<b>Info about me: </b><br>
-			<?php echo $aboutme?>
-		</div>
+			<textarea class="textarea" name="aboutme"><?php echo $aboutme ?></textarea>
+		</div>	
+	</form>
 	</div>
 </body>
-</html>
+</html> 
--- thread.php
+++ thread.php
@@ -1,4 +1,6 @@
 <?php 
+require 'menu.php';
+
 session_start();
 if(!isset($_SESSION['User_ID'])){
 	$user = 'Guest';
@@ -102,6 +104,8 @@ $posts = $row['COUNT(User_ID)'];
 	.path{
 		font-size:10pt;
 		margin-top:10px;
+		font-family:sans-serif;
+		color:black;
 	}
 	.textarea {
 		left:20%;
@@ -115,24 +119,10 @@ $posts = $row['COUNT(User_ID)'];
 </head>
 
 <body>
-		<div class="banner">
-		<div style="float:left;margin-left:7px">
-			Thread
-		</div>
-		<div align="right"  style="overflow: hidden;margin-right:7px;">
-			<?php
-				echo "Welcome ".$user;
-			?>
-		</div>
-	</div>
-	<div class="menu">
-	<a href = "index.php"> Forum </a> |
-	<a href = "profile.php"> Profile </a> |
-	<a href = "login.php"> <?php echo $login; ?> </a> | 
-	<a href = "config_page.php"> Admin Panel </a> |
-	<a href = "issues.php"> Issues </a> |
-	<a href = "contact.php"> Contact </a>
-</div>
+	<?php  
+		banner("Profile");
+		menu();
+	?>
 
 
 	<div class="path">
