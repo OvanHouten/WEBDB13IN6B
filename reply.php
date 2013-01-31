3f3bd8b2f199f0a2af527611ffbb0eb181838c3b
Kasper van Veen <kasperr_15@hotmail.com>
2013-01-31 21:41:27 +0100

    Add my version of the reply.php file

--- reply.php
+++ reply.php
@@ -1,49 +1,43 @@
 <?php
 include_once('menu.php');
 start();
+include('db.php');
 
 if(!isset($_SESSION['User_ID'])){
 	header("Location: login.php");
 }
 
-/* 
- * connectie met de sql database maken.
- */
-$dbusername='webdb13IN6B';
-$dbpassword='stafrana';
-$db = new PDO("mysql:host=localhost;dbname=webdb13IN6B;charset=UTF-8", $dbusername, $dbpassword);
-
 /*
  * Nieuwe post nummer bepalen
  */
 $post = $_REQUEST['message'];
+$thread_id = intval($_REQUEST['thread_id']);
 
-$dbpostnmr = $db->prepare('SELECT MAX(Post_number) AS lastPost FROM Replys WHERE Thread_ID = :ID');
-$dbpostnmr->bindValue(':ID', $_REQUEST['Thread_ID']);
-$dbpostnmr->execute();
-$row = $dbpostnmr->fetch();
-$postnumber = $row['lastPost'];
-++$postnumber;
+$query = 'SELECT MAX(Post_number) FROM Replys WHERE Thread_ID = :Thread_ID';
+$db_post_nr = $db->prepare($query);
+$db_post_nr->bindValue(':Thread_ID', $thread_id);
+$db_post_nr->execute();
+$tmp = $db_post_nr->fetch();
+$post_nr = $tmp[0] ? $tmp[0] + 1 : 1;
 
 /*
  * Invoegen van een nieuwe post
  */
-$dbpost = $db->prepare('INSERT INTO Replys (Text, User_ID, Thread_ID, 
-Post_number) VALUES (:message, :User_ID, :Thread_ID, :Post_number)');
+$query = 'INSERT INTO Replys (Text, User_ID, Thread_ID, 
+Post_number) VALUES (:message, :User_ID, :Thread_ID, :Post_NR)';
+
+$dbpost = $db->prepare($query);
 $dbpost->bindValue(':message', $post);
 $dbpost->bindValue(':User_ID', $_SESSION['User_ID']);
-$dbpost->bindValue(':Thread_ID', $_REQUEST['Thread_ID']);
-$dbpost->bindValue(':Post_number', $postnumber);
+$dbpost->bindValue(':Thread_ID', $thread_id);
+$dbpost->bindValue(':Post_NR', $post_nr);
 $dbpost->execute();
 
 /*
- * Text-veld mag niet leeg zijn
+ * Als het geen AJAX request is, dan wordt er doorverwezen naar de thread pagina
  */
-
 if(empty($_SERVER['HTTP_X_REQUESTED_WITH']) 
-        && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-    header("Location: thread.php?thread_id=" . $_REQUEST['Thread_ID']);
+        || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
+    header("Location: thread.php?thread_id=" . $thread_id);
 }
-
-header("Location: thread.php?thread_id=" . $_REQUEST['Thread_ID']);
 ?>
